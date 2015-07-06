8540c5b95e107d9ea17a7092774e46d267a6b912
John Chu <jgochu@gmail.com>
2015-07-06 15:40:04 -0700

    trigrams assignment

--- hw/hw13/trigrams.py
+++ hw/hw13/trigrams.py
@@ -1,24 +1,47 @@
 #!/usr/bin/env python
+import random
 
-open_file = open('sherlock-small.txt', 'r')
+open_file = open('sherlock.txt', 'r')
 text = open_file.read()
 
 open_file.close()
 
 #  remove unwanted characters
-text = text.replace('\n', ' ')
-text = text.replace('--', ' ')
-for char in ["\\", '(', ')', '-']:
+for char in ["\\", '(', ')', '-', '.', ';']:
     if char in text:
         text = text.replace(char, '')
+for char in ['\n', '--', '-']:
+    if char in text:
+        text = text.replace(char, ' ')
 
 text = text.split()
 
+
 trigrams = {}
 
-#  add words to dictionary in trigrams
-for(i, word) in enumerate(text):
-    if i in range(len(text) - 2):
-        trigrams[(text[i], text[i + 1])] = [text[i + 2]]
 
-print(trigrams)
+def words(text):
+    #  add words to dictionary in trigrams
+    for(i, word) in enumerate(text):
+        if i in range(len(text) - 2):
+            trigrams[(text[i], text[i + 1])] = [text[i + 2]]
+    return trigrams
+
+
+def text_builder(trigrams):
+
+    new_text = []
+
+    for i in range(30):
+        new_sentence = list(random.choice(list(trigrams.keys())))
+        for x in range(random.randint(3, 10)):
+            additional_words = random.choice(list(trigrams.keys()))
+            new_sentence.extend(additional_words)
+        new_sentence[0] = new_sentence[0].capitalize()
+        new_sentence[-1] += '.'
+        new_text.extend(new_sentence)
+
+    print(' '.join(new_text))
+
+words(text)
+text_builder(trigrams)
