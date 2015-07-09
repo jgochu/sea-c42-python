dfe6c8fb6ad8c38cca3b0657d21448c2dd45cab0
John Chu <jgochu@gmail.com>
2015-07-08 21:52:48 -0700

    completed first 2 steps

--- hw/hw17/html_render.py
+++ hw/hw17/html_render.py
@@ -11,12 +11,45 @@ Python class example.
 
 class Element(object):
 
-        def __init__(self, name="", content=""):
+    IND_LEVEL = "    "
+
+    def __init__(self, name="", content=""):
             self.name = name
-            self.content = content
+            self.children = [content] if content else []
 
-        def append(self, new_text):
-            self.content += new_text
+    def append(self, new_child):
+            self.children.append(new_child)
 
-        def render(self, file):
-            file.write(self.content)
+    def render(self, file_out, indent="    ", ):
+
+            file_out.write("%s<%s>\n" % (indent, self.name))
+            for child in self.children:
+                if (type(child) == str):
+                    # Add new content string without rendering
+                    file_out.write(indent + Element.IND_LEVEL + child)
+                else:
+                    # Add new child node, by recursively rendering
+                    child.render(file_out, indent + Element.IND_LEVEL)
+            file_out.write("%s</%s>\n" % (indent, self.name))
+
+
+class Html(Element):
+
+    def __init__(self, name="", content=""):
+        Element.__init__(self, name='html', content="")
+
+    def render(self, file_out, indent=""):
+        file_out.write("<!DOCTYPE html>\n")
+        Element.render(self, file_out, "")
+
+
+class Body(Element):
+
+    def __init__(self, content=""):
+        Element.__init__(self, name='body', content=content)
+
+
+class P(Element):
+
+    def __init__(self, content=""):
+        Element.__init__(self, name='p', content=content)
